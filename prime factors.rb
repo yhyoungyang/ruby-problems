917995a167c5f09dff0f27671c2b016f56384d7b
youngyang <yyangag@gmail.com>
2015-05-04 17:09:12 -0700

    un-commented prime factor method 1 solution

--- prime factors.rb
+++ prime factors.rb
@@ -2,12 +2,12 @@
 #Write a function to find all prime factors of a number
 
 #method 1
-# require 'prime'
-# def primeFactors(number)
-#   Prime.prime_division(number).flat_map { | factor, power | [factor] * power }
-# end
+require 'prime'
+def primeFactors(number)
+  Prime.prime_division(number).flat_map { | factor, power | [factor] * power }
+end
 
-# puts primeFactors(128)
+puts primeFactors(128)
 
 #method 2
 
