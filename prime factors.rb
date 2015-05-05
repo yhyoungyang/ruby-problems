6ad655cdbdf4dc02123dd2296b507e553a6c8fb3
youngyang <yyangag@gmail.com>
2015-05-04 20:44:22 -0700

    Key learnings and method 3 added to prime factors

--- prime factors.rb
+++ prime factors.rb
@@ -19,3 +19,22 @@ end
 
 puts primeFactors(128)
 # only gives the first level of factorization 
+
+#method 3
+
+def primeFactors(number)
+  factors =[]
+  while (number > 1) do 
+    i = 2
+    while i <= number*2/3 && number%i != 0 do 
+      i += 1
+    end
+    factors << i 
+    number /= i 
+  end 
+  factors.sort
+end
+
+#2 key learnings
+# 1. << ----------------------> = .push into an array, append 
+# 2. /= ----------------------> number = number / divisor 
